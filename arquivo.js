f1b6423cf9ec695c2faef0b7fc1a730a7fa2bef5
Leandro Veras Padilha Filho <lvpf@cesar.school>
2024-11-27 23:49:28 -0300

    fim

--- arquivo.js
+++ arquivo.js
@@ -1,16 +1,15 @@
-// Obtém o botão e o corpo do documento
 const botaoModo = document.getElementById('modo-toggle');
 const body = document.body;
 
-// Função para alternar entre os modos
+
 botaoModo.addEventListener('click', function() {
-    // Alterna a classe 'modo-escuro' no corpo da página
+
     body.classList.toggle('modo-escuro');
 
-    // Altera o texto do botão com base no estado atual
+
     if (body.classList.contains('modo-escuro')) {
-        botaoModo.textContent = 'Ativar Modo Claro'; // Modo escuro ativado
+        botaoModo.textContent = 'Ativar Modo Claro';
     } else {
-        botaoModo.textContent = 'Ativar Modo Escuro'; // Modo claro ativado
+        botaoModo.textContent = 'Ativar Modo Escuro';
     }
 });
